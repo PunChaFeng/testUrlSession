257ad7d72cf6d956424722d0b3930fdcff940a43
Xiaofeng Feng <Xiaofeng.feng@autodesk.com>
2022-12-14 19:53:59 +0800

    Initial Commit

---
+++ testUrlSession/testUrlSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  testUrlSessionApp.swift
+//  testUrlSession
+//
+//  Created by Xiaofeng Feng on 2022/12/14.
+//
+
+import SwiftUI
+
+@main
+struct testUrlSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
